16d0b2d3441268a532371cada05f253c4a3903d3
jayasaikrishnayerramsetty <saikrishnayerramsetty@kodekloud.com>
2024-10-10 19:08:16 +0530

    Updated the workflow

--- main.tf
+++ main.tf
@@ -3,13 +3,14 @@ provider "aws" {
 }
 
 resource "aws_lambda_function" "example_lambda" {
-  filename      = "lambda_function.zip"
-  function_name = var.lambda_function_name
-  role          = aws_iam_role.lambda_role.arn
-  handler       = "lambda_function.lambda_handler"
-  runtime       = "python3.12"
-
+  filename         = "lambda_function.zip"
+  function_name    = var.lambda_function_name
+  role             = aws_iam_role.lambda_role.arn
+  handler          = "lambda_function.lambda_handler"
+  runtime          = "python3.12"
   source_code_hash = filebase64sha256("lambda_function.zip")
+
+  depends_on = [aws_iam_role_policy_attachment.lambda_logs]
 }
 
 resource "aws_iam_role" "lambda_role" {
@@ -27,4 +28,40 @@ resource "aws_iam_role" "lambda_role" {
       }
     ]
   })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.lambda_logging.arn
+}
+
+resource "aws_iam_policy" "lambda_logging" {
+  name        = "lambda_logging_policy"
+  path        = "/"
+  description = "IAM policy for logging from a lambda"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Resource = "arn:aws:logs:*:*:*"
+      }
+    ]
+  })
+}
+
+variable "aws_region" {
+  description = "The AWS region to deploy to"
+  default     = "us-east-1"
+}
+
+variable "lambda_function_name" {
+  description = "The name of the Lambda function"
+  default     = "example_lambda_function"
 }
