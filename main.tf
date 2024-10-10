cc7c989bc8dc712882ef84939455601d15de5927
jayasaikrishnayerramsetty <saikrishnayerramsetty@kodekloud.com>
2024-10-10 19:24:29 +0530

    Modified the lambda function

--- main.tf
+++ main.tf
@@ -55,13 +55,3 @@ resource "aws_iam_policy" "lambda_logging" {
     ]
   })
 }
-
-variable "aws_region" {
-  description = "The AWS region to deploy to"
-  default     = "us-east-1"
-}
-
-variable "lambda_function_name" {
-  description = "The name of the Lambda function"
-  default     = "example_lambda_function"
-}
