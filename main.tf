de97fcde929bc77f59bcb201e7791250ec88dce2
jayasaikrishnalp <79148878+jayasaikrishnalp@users.noreply.github.com>
2024-10-22 11:36:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,15 +16,3 @@ resource "aws_lambda_function" "example_lambda" {
   runtime         = "python3.12"
   source_code_hash = filebase64sha256("lambda_function.zip")
 }
-
-variable "aws_region" {
-  description = "AWS region"
-  type        = string
-  default     = "us-west-2"
-}
-
-variable "lambda_function_name" {
-  description = "Name of the lambda function"
-  type        = string
-  default     = "example_lambda_function"
-}
