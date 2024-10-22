ab64bef7a5db569698e0d4e7ab0357cd90b8599c
jayasaikrishnalp <79148878+jayasaikrishnalp@users.noreply.github.com>
2024-10-22 11:45:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,10 +7,11 @@ data "aws_iam_role" "lambda_role" {
 }
 
 resource "aws_lambda_function" "example_lambda" {
-  filename         = "lambda_function.zip"
-  function_name    = var.lambda_function_name
-  role            = data.aws_iam_role.lambda_role.arn
-  handler         = "lambda_function.lambda_handler"
-  runtime         = "python3.12"
+  filename      = "lambda_function.zip"
+  function_name = var.lambda_function_name
+  role          = data.aws_iam_role.lambda_role.arn
+  handler       = "lambda_function.lambda_handler"
+  runtime       = "python3.12"
+
   source_code_hash = filebase64sha256("lambda_function.zip")
 }
