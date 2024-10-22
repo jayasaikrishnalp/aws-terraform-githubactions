e8ec4ec7542d333beaca4fc81cf394db0be1b8e3
jayasaikrishnalp <79148878+jayasaikrishnalp@users.noreply.github.com>
2024-10-22 11:16:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,12 +2,38 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_lambda_function" "example_lambda" {
-  filename      = "lambda_function.zip"
-  function_name = var.lambda_function_name
-  role          = var.lambda_role_arn
-  handler       = "lambda_function.lambda_handler"
-  runtime       = "python3.12"
+resource "aws_iam_role" "lambda_role" {
+  name = "example_lambda_role"
 
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+# Basic Lambda execution policy
+resource "aws_iam_role_policy_attachment" "lambda_basic" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  role       = aws_iam_role.lambda_role.name
+}
+
+resource "aws_lambda_function" "example_lambda" {
+  filename         = "lambda_function.zip"
+  function_name    = var.lambda_function_name
+  role            = aws_iam_role.lambda_role.arn
+  handler         = "lambda_function.lambda_handler"
+  runtime         = "python3.12"
   source_code_hash = filebase64sha256("lambda_function.zip")
+
+  depends_on = [
+    aws_iam_role_policy_attachment.lambda_basic
+  ]
 }
